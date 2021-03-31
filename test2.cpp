a9deb5f50f327599099608090fea11643512e6cb
ThinhFullHD <81562297+ThinhFullHD@users.noreply.github.com>
2021-04-01 00:21:27 +0700

    updated Creation of Course

--- test2.cpp
+++ test2.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-
-struct Sem{
-    int year;
-    string SemStart;
-    string SemEnd;
-    string regisStart;
-    string regisEnd;
-    Course s;
-    Course* head=nullptr;
-}
 struct Course{
     int CoId;
     string CoName;
@@ -19,12 +10,25 @@ struct Course{
     string day;
     string s;
     Course*next;
-}
+};
+struct Sem{
+    int year;
+    string SemStart;
+    string SemEnd;
+    string regisStart;
+    string regisEnd;
+    Course s;
+    Course*head=nullptr;
+    Course*cur=nullptr;
+};
+
+void create_courselist(Sem &s, Sem srr[]);
+void displaytest(Sem s, Sem srr[], int i, int year);
 
 void create_courselist(Sem &s, Sem srr[]){
     int year, i;
     cout << "==========================================" << endl;
-    cout << "=  Create a List of Course for this Sem  ="
+    cout << "=  Create a List of Course for this Sem  =" << endl;
     cout << "==========================================" << endl;
     cout << "The Year of Current Sem: "; cin >> year;
     cout << "---------------------------" << endl;
@@ -44,23 +48,37 @@ void create_courselist(Sem &s, Sem srr[]){
     cout << "---------------------------" << endl;
     cout << "When the registion start and end?" << endl;
     cout << "Start in: ";
-    getline(cin, regisStart);
+    getline(cin, srr[i].regisStart);
     cout << "End in: ";
-    getline(cin, regisEnd);
+    getline(cin, srr[i].regisEnd);
     cout << "---------------------------" << endl;
     cout << "Enter 0 in No to stop!" << endl;
     int No;
     cout << "No: "; cin>>No;
-    Course*cur=nullptr;
+    srr[i].cur=nullptr;
     while (No!=0){
-        if (head==nullptr){
-            head= new Course;
-            cur=head;
+        if (srr[i].head==nullptr){
+            srr[i].head= new Course;
+            srr[i].cur=srr[i].head;
         } else {
-            cur->next=new Course;
-            cur=cur->next;
-        } cout << ""
+            srr[i].cur->next=new Course;
+            srr[i].cur=srr[i].cur->next;
+        } cout << "Id of Course: ";
+        cin >> srr[i].cur->CoId;
+        cin.ignore(32767, '\n');
+        cout << "Name of course: ";
+        getline(cin, srr[i].cur->CoName);
+        cout << "Number of credits: "; 
+        cin >> srr[i].cur->credit;
+        cin.ignore(32767, '\n');
+        cout << "Day of week: ";
+        getline(cin, srr[i].cur->day);
+        cout << "Seaon of day: ";
+        getline(cin, srr[i].cur->s);
+        srr[i].cur->next=nullptr;
+        cout << "No: "; cin>>No;
     }
+    displaytest(s, srr, i, year);
 }
 /*struct Sem{
     int year;
@@ -80,8 +98,27 @@ struct Course{
     string s;
 }*/
 
+void displaytest(Sem s, Sem srr[], int i, int year){
+    cout << setfill(' ');
+    cout << "Course List of Sem " << i+1 << " in the year of " << year << " ,which from ";
+    cout << srr[i].SemStart << " to " << srr[i].SemEnd << endl;
+    cout << "==========================================================================================" << endl;
+    cout << "Registion time is from " << srr[i].regisStart << " to " << srr[i].regisEnd << endl;
+    cout << "==========================================================================================" << endl;
+    cout << setw(5) << "ID";
+    cout << setw(15) << "Name";
+    cout << setw(15) << "Teacher";
+    cout << setw(5)  << "Credit";
+    cout << setw(5)  << "Maximum";
+    cout << setw(15) << "Day";
+    cout << setw(15) << "Season"; 
+}
+
+
 int main(){
     Sem s;
     Sem srr[3];
     create_courselist(s, srr);
+    return 0;
 }
+
