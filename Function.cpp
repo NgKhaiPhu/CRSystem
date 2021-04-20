684e11f55cd1a68169c2a1d7a3bc9dd55f9a5cbe
NgKhaiPhu <nkphu243373@gmail.com>
2021-04-20 22:29:22 +0700

    Update Function.cpp

--- Function.cpp
+++ Function.cpp
@@ -1,29 +1,87 @@
 #include <iostream>
 using namespace std;
 
-void login() {};
+struct date
+{
+	int day;
+	int month;
+	int year;
+};
+
+struct student
+{
+	int no;
+	int id;
+	string firstname;
+	string lastname;
+	string password;
+	string sclass;
+	string gender;
+	const int type = 0;
+	int socialid;
+	student* next;
+	date dob;
+};
+
+struct staff
+{
+	string fullname;
+	string gender;
+	string password;
+	string username;
+	const int type = 1;
+};
+
+struct Course {
+	int CoId;
+	string CoName;
+	string teacherName;
+	int credit;
+	int maxSt = 50;
+	string day[2];
+	string s[2];
+	Course* next;
+};
+struct Sem {
+	int year;
+	string SemStart;
+	string SemEnd;
+	string regisStart;
+	string regisEnd;
+	Course* head = nullptr;
+	Course* cur = nullptr;
+};
+
+void inputAcc(fstream& acc, student*& a);
+void login(student* a, string& usn);
+void deleteList(student*& a);
+void display(ifstream& ifs, string u);
+void changepass(fstream& fs, string u, student* a);
 
 //1
-string newSchoolYr() {};//2020-2021
-void newclass() {};//20APCS1
-void addFreshman() {};//(CSV) 1,20127062,Phu,Nguyen,Male,13/05/2002,079202005145
+bool check_file_exist(string* file);
+void create_class(student*& head);
+void import_list(student*& head, string* severalclass);
+void add_class_list(string* severalclass);
+void create_school_year();
 
 //2 
-void newSem() {};//1, 2020, 27/10, 31/12
-void newCRsession() {};//20/10, 26/10
-void create_courselist() {};//CSC10001, Programming Basics, Ngo Dinh Hy, 4, 50, 3-1, 5-3, 5-4
-void displayCourseList() {};//5
-void upCourse() {};
-void deleteCourse() {};
+void TCmenu(Sem se, Sem srr[], int i, int year, int season);
+void create_courselist(Sem& se, Sem srr[], int& i, int& year, int& season);
+void displayCourseList(Sem se, Sem srr[], int i, int year, int season);
+void deleteCourse(Sem& se, Sem srr[], int i);
 
 //3
-void enroll() {};//conflict, max 5
-void viewEnrolled() {};//4
-void delEnrolled() {};
+void InputCourse(fstream& f, Sem& S, int& y);
+void EnrollCourse(Sem S, Course*& MyCourse);
+void ViewAvailableCourse(Sem S, int y);
+void ViewMyCourse(Sem S, int y, Course* MyCo);
+void DeleteCourse(Sem& S, Course*& MyCo);
+void OperateTask(Sem& S, Course*& MyCourse, int& y);
 
 //5
 void viewClass() {};
-void viewClassStu() {};
+void ViewStudentofClass(student* head);
 void viewCourseStu() {};
 
 //6
